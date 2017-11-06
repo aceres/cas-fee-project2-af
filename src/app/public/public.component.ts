672eaf1d45809cdb164e2105597eef0b65a1e297
Tanja Sennhauser <tanja.sennhauser@nobel-web-engineering.ch>
2017-11-06 22:19:12 +0100

    cleaned up public.component

--- src/app/public/public.component.ts
+++ src/app/public/public.component.ts
@@ -1,34 +1,51 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import {AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2/database';
 import { Router } from '@angular/router';
 
+import { RecipeService } from '../services/recipe.service';
+
 @Component({
-  selector: 'app-public',
-  templateUrl: './public.component.html',
-  styleUrls: ['./public.component.less']
+    selector: 'app-public',
+    templateUrl: './public.component.html',
+    styleUrls: ['./public.component.less']
 })
 export class PublicComponent {
-  title = 'Manducare';
+    title = 'Manducare';
 
-  allRecipes: FirebaseListObservable<any[]>;
+    allRecipes: FirebaseListObservable<any[]>;
+    getAllRecipes;
+    calculateAllRecipes;
+    randomRecipe;
 
-  constructor(
-    private router: Router,
-    db: AngularFireDatabase
-  ) {
-      this.allRecipes = db.list('/recipes', {
-        query: {
-          orderByChild: 'rating'
-        }
-      })
-      .map(items => items.sort((a, b) => b.rating - a.rating)) as FirebaseListObservable<any[]>;
-  }
+    constructor(
+        private router: Router,
+        db: AngularFireDatabase,
+        private recipeService: RecipeService,
+    ) {
+        this.allRecipes = db.list('/recipes', {
+            query: {
+                orderByChild: 'rating'
+            }
+        })
+            .map(items => items.sort((a, b) => b.rating - a.rating)) as FirebaseListObservable<any[]>;
 
-  detail(recipe): void {
-    this.router.navigate(['/recipe-detail', recipe.$key]);
-  }
+        // Get the newest recipe
+        this.randomRecipe = db.list('/recipes', {
+            query: {
+                limitToLast: 1
+            }
+        })
+            .subscribe(item => {
+                this.randomRecipe = item[0];
+                console.log('randomRecipe: ', this.randomRecipe);
+            })
+    }
 
-  listRecipeSpecificCategory(category): void {
-    this.router.navigate(['/category-list', category]);
-  }
+    detail(recipe): void {
+        this.router.navigate(['/recipe-detail', recipe.$key]);
+    }
+
+    listRecipeSpecificCategory(category): void {
+        this.router.navigate(['/category-list', category]);
+    }
 }
