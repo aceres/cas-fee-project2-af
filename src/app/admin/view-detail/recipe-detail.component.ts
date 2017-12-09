b4e685b4d15b8a00b292f50a30acd99c80d6028f
André Ceres <andre.ceres@gmail.com>
2017-12-09 21:39:37 +0100

    -- show the full name of category, cuisine and level in the detail (admin)

--- src/app/admin/view-detail/recipe-detail.component.ts
+++ src/app/admin/view-detail/recipe-detail.component.ts
@@ -4,6 +4,9 @@ import { Location } from '@angular/common';
 import 'rxjs/add/operator/switchMap';
 import { Recipe } from '../../services/models/recipe';
 import { RecipeService } from '../../services/recipe.service';
+import { listLevels } from '../../global/list.levels';
+import { listCuisines } from '../../global/list.cuisines';
+import { listCategories } from '../../global/list.categories';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -14,6 +17,9 @@ export class RecipeDetailComponent implements OnInit {
 
   recipe: Recipe;
   id;
+  levelFullName;
+  cuisineFullName;
+  categoryFullName;
 
   constructor(
     private recipeService: RecipeService,
@@ -24,7 +30,24 @@ export class RecipeDetailComponent implements OnInit {
   ngOnInit(): void {
     this.route.paramMap
       .switchMap((params: ParamMap) => this.recipeService.getRecipe(params.get('id')))
-      .subscribe(recipe => this.recipe = recipe);
+      .subscribe(recipe => {
+        this.recipe = recipe
+          for (let i = 0; i < listLevels.length; i++) {
+              if (listLevels[i].value === this.recipe.level) {
+                  this.levelFullName = listLevels[i].display;
+              }
+          }
+          for (let i = 0; i < listCuisines.length; i++) {
+              if (listCuisines[i].value === this.recipe.cuisine) {
+                  this.cuisineFullName = listCuisines[i].name;
+              }
+          }
+          for (let i = 0; i < listCategories.length; i++) {
+              if (listCategories[i].value === this.recipe.category) {
+                  this.categoryFullName = listCategories[i].name;
+              }
+          }
+      });
 
       this.id = this.route.snapshot.params['id'];
       console.log('this.id', this.id);
